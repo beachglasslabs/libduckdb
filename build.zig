f0ebef527a066b35b293962e536605520197380f
Ed Yu <edyu@users.noreply.github.com>
2023-06-23 14:26:48 -0700

    Add step to build artifact

--- build.zig
+++ build.zig
@@ -8,17 +8,28 @@ pub fn build(b: *std.Build) void {
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
-    //const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{});
 
     // Standard optimization options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
-    //const optimize = b.standardOptimizeOption(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "duckdb",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    lib.addIncludePath("include");
+    lib.addLibraryPath("lib");
+    lib.linkSystemLibraryName("duckdb");
 
     b.installFile("include/duckdb.h", "include/duckdb.h");
-    b.installLibFile("lib/libduckdb.so", "libduckdb.so");
+    //b.installLibFile("lib/libduckdb.so", "libduckdb.so");
 
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
+    b.installArtifact(lib);
 }
