513f4f217a9ba595824ccb94d05a3491f380fd94
Ed Yu <edyu@users.noreply.github.com>
2023-06-23 20:21:05 -0700

    Add libduckdb module

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -14,6 +14,10 @@ pub fn build(b: *std.Build) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
+
+    var libduckdb_module = b.createModule(.{ .source_file = .{ .path = "lib/libduckdb.so" } });
+
+    try b.modules.put(b.dupe("libduckdb"), libduckdb_module);
 
     const lib = b.addStaticLibrary(.{
         .name = "duckdb",
