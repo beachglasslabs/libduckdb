cbd94582b4be0b0cb262dcb8eb13050292169c90
Ed Yu <edyu@users.noreply.github.com>
2023-10-18 09:43:15 -0700

    Add modules to expose header and shared library

--- build.zig
+++ build.zig
@@ -4,6 +4,8 @@ const std = @import("std");
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) !void {
+    _ = b.addModule("libduckdb.so", .{ .source_file = .{ .path = "lib" } });
+    _ = b.addModule("duckdb.h", .{ .source_file = .{ .path = "include" } });
     const lib_install_step = b.addInstallLibFile(.{ .path = "lib/libduckdb.so" }, "libduckdb.so");
     const header_install_step = b.addInstallHeaderFile("include/duckdb.h", "duckdb.h");
     b.getInstallStep().dependOn(&lib_install_step.step);
