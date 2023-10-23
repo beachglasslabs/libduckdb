f55b2dc28f115d4a1b96fb93f6deaf5269fbab8a
Ed Yu <edyu@users.noreply.github.com>
2023-10-23 15:54:22 -0700

    Add static library

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # libduckdb
 
-A place to put the libduckdb dependency.
+A place to put the duckdb dependency.
+The files are from the the release archives released by [duckdb](https://github.com/duckdb/duckdb/).
--- build.zig
+++ build.zig
@@ -1,13 +1,10 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) !void {
-    _ = b.addModule("libduckdb.lib", .{ .source_file = .{ .path = b.pathFromRoot("lib") } });
+    _ = b.addModule("libduckdb.library", .{ .source_file = .{ .path = b.pathFromRoot("lib") } });
     _ = b.addModule("libduckdb.include", .{ .source_file = .{ .path = b.pathFromRoot("include") } });
-    _ = b.addModule("duckdb.h", .{ .source_file = .{ .path = b.pathFromRoot("include/duckdb.h") } });
-    _ = b.addModule("libduckdb.so", .{ .source_file = .{ .path = b.pathFromRoot("lib/libduckdb.so") } });
+    _ = b.addModule("libduckdb.h", .{ .source_file = .{ .path = b.pathFromRoot("include/duckdb.h") } });
+    _ = b.addModule("libduckdb.a", .{ .source_file = .{ .path = b.pathFromRoot("lib/libduckdb_static.a") } });
     // const lib_install_step = b.addInstallLibFile(.{ .path = "lib/libduckdb.so" }, "libduckdb.so");
     // const header_install_step = b.addInstallHeaderFile("include/duckdb.h", "duckdb.h");
     // b.getInstallStep().dependOn(&lib_install_step.step);
